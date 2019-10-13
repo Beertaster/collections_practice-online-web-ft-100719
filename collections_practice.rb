b9cdb1a6543eb130e1b38bf76029510ed404ca45
William Stevens <wstevens.hornet@gmail.com>
2019-10-13 05:34:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,11 +21,11 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  kesha = []
+  arr2 = []
   array.each do |word|
-    word_array = word.split ""
-    word_array[2] = "$"
-    kesha << word_array.join
+    arr = word.split ""
+    arr[2] = "$"
+    arr2 << word_array.join
   end
-  kesha
+  arr2
 end
